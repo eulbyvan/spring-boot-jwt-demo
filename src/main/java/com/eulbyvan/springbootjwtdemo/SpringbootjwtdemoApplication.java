11c648d7ae90907fe354138bb1e334fbc7a6a446
eulbyvan <work.issdq@gmail.com>
2022-11-29 05:05:36 +0700

    initialized a spring project

---
+++ src/main/java/com/eulbyvan/springbootjwtdemo/SpringbootjwtdemoApplication.java
@@ -0,0 +1,13 @@
+package com.eulbyvan.springbootjwtdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootjwtdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootjwtdemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eulbyvan/springbootjwtdemo/SpringbootjwtdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eulbyvan.springbootjwtdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootjwtdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
